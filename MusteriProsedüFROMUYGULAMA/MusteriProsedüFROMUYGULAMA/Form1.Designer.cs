384debe799d19d3b988ecafdb7e37bf406561e24
burhankskn <burhankeskin1234@gmail.com>
2022-03-22 15:34:53 +0300

    Eklendi

---
+++ MusteriProsedüFROMUYGULAMA/MusteriProsedüFROMUYGULAMA/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿
+namespace MusteriProsedüFROMUYGULAMA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Adıtxt = new System.Windows.Forms.TextBox();
+            this.Soyadıtxt = new System.Windows.Forms.TextBox();
+            this.Adrestxt = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.aramaAdıtxt = new System.Windows.Forms.TextBox();
+            this.Kaydetbtn = new System.Windows.Forms.Button();
+            this.Silbtn = new System.Windows.Forms.Button();
+            this.Vazgeçbtn = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.aramabtn = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Book Antiqua", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(374, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(162, 27);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Müşteri Listesi";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Book Antiqua", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(26, 123);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 27);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Soyad:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Book Antiqua", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(26, 169);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 27);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Adres:";
+            // 
+            // Adıtxt
+            // 
+            this.Adıtxt.Font = new System.Drawing.Font("Book Antiqua", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Adıtxt.Location = new System.Drawing.Point(132, 78);
+            this.Adıtxt.Name = "Adıtxt";
+            this.Adıtxt.Size = new System.Drawing.Size(181, 27);
+            this.Adıtxt.TabIndex = 1;
+            // 
+            // Soyadıtxt
+            // 
+            this.Soyadıtxt.Font = new System.Drawing.Font("Book Antiqua", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Soyadıtxt.Location = new System.Drawing.Point(132, 123);
+            this.Soyadıtxt.Name = "Soyadıtxt";
+            this.Soyadıtxt.Size = new System.Drawing.Size(181, 27);
+            this.Soyadıtxt.TabIndex = 1;
+            // 
+            // Adrestxt
+            // 
+            this.Adrestxt.Font = new System.Drawing.Font("Book Antiqua", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Adrestxt.Location = new System.Drawing.Point(132, 169);
+            this.Adrestxt.Name = "Adrestxt";
+            this.Adrestxt.Size = new System.Drawing.Size(181, 27);
+            this.Adrestxt.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Book Antiqua", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(308, 25);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 27);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Adı:";
+            // 
+            // aramaAdıtxt
+            // 
+            this.aramaAdıtxt.Font = new System.Drawing.Font("Book Antiqua", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.aramaAdıtxt.Location = new System.Drawing.Point(379, 27);
+            this.aramaAdıtxt.Name = "aramaAdıtxt";
+            this.aramaAdıtxt.Size = new System.Drawing.Size(354, 27);
+            this.aramaAdıtxt.TabIndex = 1;
+            // 
+            // Kaydetbtn
+            // 
+            this.Kaydetbtn.BackColor = System.Drawing.Color.Lime;
+            this.Kaydetbtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Kaydetbtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Kaydetbtn.Location = new System.Drawing.Point(17, 241);
+            this.Kaydetbtn.Name = "Kaydetbtn";
+            this.Kaydetbtn.Size = new System.Drawing.Size(75, 35);
+            this.Kaydetbtn.TabIndex = 2;
+            this.Kaydetbtn.Text = "Kaydet";
+            this.Kaydetbtn.UseVisualStyleBackColor = false;
+            this.Kaydetbtn.Click += new System.EventHandler(this.Kaydetbtn_Click);
+            // 
+            // Silbtn
+            // 
+            this.Silbtn.BackColor = System.Drawing.Color.Red;
+            this.Silbtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Silbtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Silbtn.Location = new System.Drawing.Point(123, 239);
+            this.Silbtn.Name = "Silbtn";
+            this.Silbtn.Size = new System.Drawing.Size(75, 35);
+            this.Silbtn.TabIndex = 2;
+            this.Silbtn.Text = "Sil";
+            this.Silbtn.UseVisualStyleBackColor = false;
+            this.Silbtn.Click += new System.EventHandler(this.Silbtn_Click);
+            // 
+            // Vazgeçbtn
+            // 
+            this.Vazgeçbtn.BackColor = System.Drawing.Color.Gray;
+            this.Vazgeçbtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Vazgeçbtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Vazgeçbtn.Location = new System.Drawing.Point(224, 239);
+            this.Vazgeçbtn.Name = "Vazgeçbtn";
+            this.Vazgeçbtn.Size = new System.Drawing.Size(89, 35);
+            this.Vazgeçbtn.TabIndex = 2;
+            this.Vazgeçbtn.Text = "Vazgeç";
+            this.Vazgeçbtn.UseVisualStyleBackColor = false;
+            this.Vazgeçbtn.Click += new System.EventHandler(this.Vazgeçbtn_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(338, 121);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(604, 301);
+            this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.DoubleClick += new System.EventHandler(this.dataGridView1_DoubleClick);
+            // 
+            // aramabtn
+            // 
+            this.aramabtn.BackColor = System.Drawing.Color.Lime;
+            this.aramabtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.aramabtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.aramabtn.Location = new System.Drawing.Point(752, 25);
+            this.aramabtn.Name = "aramabtn";
+            this.aramabtn.Size = new System.Drawing.Size(86, 29);
+            this.aramabtn.TabIndex = 2;
+            this.aramabtn.Text = "Arama";
+            this.aramabtn.UseVisualStyleBackColor = false;
+            this.aramabtn.Click += new System.EventHandler(this.aramabtn_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Book Antiqua", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(49, 78);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 27);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Adı:";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(958, 22);
+            this.statusStrip1.TabIndex = 4;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(958, 450);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.Vazgeçbtn);
+            this.Controls.Add(this.Silbtn);
+            this.Controls.Add(this.aramabtn);
+            this.Controls.Add(this.Kaydetbtn);
+            this.Controls.Add(this.Adrestxt);
+            this.Controls.Add(this.Soyadıtxt);
+            this.Controls.Add(this.aramaAdıtxt);
+            this.Controls.Add(this.Adıtxt);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox Adıtxt;
+        private System.Windows.Forms.TextBox Soyadıtxt;
+        private System.Windows.Forms.TextBox Adrestxt;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox aramaAdıtxt;
+        private System.Windows.Forms.Button Kaydetbtn;
+        private System.Windows.Forms.Button Silbtn;
+        private System.Windows.Forms.Button Vazgeçbtn;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button aramabtn;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+    }
+}
+
---
+++ MusteriProsedüFROMUYGULAMA/MusteriProsedüFROMUYGULAMA/Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Dapper;
+using System.Data.SqlClient;
+
+namespace MusteriProsedüFROMUYGULAMA
+{
+    public partial class Form1 : Form
+    {
+
+        SqlConnection sqlCon = new SqlConnection("Data Source=DESKTOP-ICU6LR7;Initial Catalog=Musteriler;Integrated Security=True");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //id degişkeninin veri kayıtlarındaki id sunutu için kullanılır
+        int id = 0;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                FillDataGridView();
+                Clear();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                throw;
+            }
+        }
+
+        private void Clear()
+        {
+            //throw new NotImplementedException();
+        }
+
+        private void Kaydetbtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (sqlCon.State == ConnectionState.Closed)
+                {
+                    sqlCon.Open();
+                    //MessageBox.Show("Bağlandı");
+                }
+                DynamicParameters param = new DynamicParameters();
+                param.Add("@id", id, DbType.Int32, ParameterDirection.Input, ((short)id));
+                param.Add("@Adı", Adıtxt.Text.Trim());
+                param.Add("@soyadı", Soyadıtxt.Text.Trim());
+                param.Add("@sil", false);
+                param.Add("@Adres", Adrestxt.Text.Trim());
+                sqlCon.Execute
+                    ("MusteriekleveDuzenle", param, commandType: CommandType.StoredProcedure);
+                if (id == 0)
+                {
+                    toolStripStatusLabel1.Text = "Kayıt oldu";
+                }
+                else
+                {
+                    toolStripStatusLabel1.Text = "Güncelleme oldu";
+                }
+                FillDataGridView();
+                clear();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                throw;
+            }
+            finally
+            {
+                sqlCon.Close();
+            }
+
+           
+
+
+
+        }
+
+
+        void FillDataGridView()
+        {
+            DynamicParameters param = new DynamicParameters();
+            param.Add("@Aramametni", aramaAdıtxt.Text.Trim());
+            List<MusteriBigileri> list = sqlCon.Query<MusteriBigileri>
+                ("MusteriArama", param, commandType:CommandType.StoredProcedure).ToList<MusteriBigileri>();
+            dataGridView1.DataSource = list;
+            dataGridView1.Columns[0].Visible = false;
+            dataGridView1.Columns[3].Visible = false;
+
+
+        }
+        class MusteriBigileri
+        {
+            public int id { get; set; }
+
+            public string Adı { get; set; }
+            public string soyadı { get; set; }
+
+            public bool sil { get; set; }
+
+            public string Adres { get; set; }
+
+
+        }
+
+        private void aramabtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                FillDataGridView();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                throw;
+            }
+        }
+        //Vazgeç
+        private void Vazgeçbtn_Click(object sender, EventArgs e)
+        {
+            clear();
+        }
+
+         void clear()
+        {
+            Adıtxt.Text = "";
+            Soyadıtxt.Text = "";
+            Adrestxt.Text = "";
+            id = 0;
+            Kaydetbtn.Text = "Kaydet";
+            Silbtn.Enabled = false;
+
+        }
+
+        private void dataGridView1_DoubleClick(object sender, EventArgs e)
+        {
+            try
+            {
+                if (dataGridView1.CurrentRow.Index != -1)
+                {
+                    id = int.Parse(dataGridView1.CurrentRow.Cells[0].Value.ToString());
+                    Adıtxt.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+                    Soyadıtxt.Text =  dataGridView1.CurrentRow.Cells[2].Value.ToString();
+                    Adrestxt.Text =  dataGridView1.CurrentRow.Cells[4].Value.ToString();
+                    Silbtn.Enabled = true;
+                    Kaydetbtn.Text = "Düzenle";
+
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Silbtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DynamicParameters param = new DynamicParameters();
+                param.Add("@id",id);
+                sqlCon.Execute("MüşteriSilYeni",param,commandType:
+                    CommandType.StoredProcedure);
+                Clear();
+                FillDataGridView();
+                toolStripStatusLabel1.Text = "Silme başarılı";
+                 
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                
+            }
+        }
+    }
+}
